47a0f35d0e2303cf51460d8342825e1ecab87270
Gautam Kumar Pandey <pandeygautam748@gmail.com>
2026-01-11 23:13:06 +0530

    Find peak element in array

---
+++ src/com/array/FindPeakElement.java
@@ -0,0 +1,23 @@
+package com.array;
+
+public class FindPeakElement {
+    public static int findPeakElement(int[] nums) {
+
+        int start =0;
+        int end =nums.length-1;
+        while(start<end){
+            int mid =start+(end-start)/2;
+            if(nums[mid]>nums[mid+1]){
+                end =mid;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return end;
+    }
+    public static void main(String[] args) {
+        int result = findPeakElement(new int[]{1,2,1,3,5,6,4});
+        System.out.println(result);
+    }
+}
