94a0b413fc49703064f41c3219af1481152054d1
Gautam Kumar Pandey <pandeygautam748@gmail.com>
2026-01-26 00:33:59 +0530

    Find Duplicate

---
+++ src/com/array/FindDuplicate.java
@@ -0,0 +1,33 @@
+package com.array;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class FindDuplicate {
+    public static boolean getDuplicate(int nums[]){
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1])
+                return true;
+        }
+        return false;
+    }
+    public static boolean containsDuplicate(int[] nums) {
+
+        Set<Integer> set = new HashSet<>();
+        for(int n:nums){
+            if(!set.add(n)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(containsDuplicate(new int[]{1,2,3,1}));
+        System.out.println(getDuplicate(new int[]{1,2,3,4,1}));
+    }
+
+}
