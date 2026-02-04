d43544438bb3b35f7e81aca765997e556b9cece1
Gautam Kumar Pandey <pandeygautam748@gmail.com>
2026-02-05 00:11:23 +0530

    Find single number using XOR

--- src/com/array/FindSingleNumberUsingXor.java
+++ src/com/array/FindSingleNumberUsingXor.java
@@ -1,4 +1,14 @@
 package com.array;
 
 public class FindSingleNumberUsingXor {
+    public static int singleNumber(int[] nums) {
+        int res =0;
+        for(int i=0;i<nums.length;i++){
+            res^=nums[i];
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        System.out.println(singleNumber(new int[]{1,2,2,1,3,3,4}));
+    }
 }
