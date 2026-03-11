523466b511cf186d44a415d57d2b724b2b70dc63
Gautam Kumar Pandey <pandeygautam748@gmail.com>
2026-03-11 18:13:10 +0530

    Find Continuous sub array with optimized way

--- src/com/array/medium/ContinuousSubArrayOptimized.java
+++ src/com/array/medium/ContinuousSubArrayOptimized.java
@@ -1,4 +1,32 @@
 package com.array.medium;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ContinuousSubArrayOptimized {
+    public static boolean checkSubarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int prefixSum = 0;
+        map.put(0, -1);
+        for (int i = 0; i < nums.length; i++) {
+            {
+                prefixSum += nums[i];
+                int module = prefixSum % k;
+                if (map.containsKey(module)) {
+                    if (i - (map.get(module)) >= 2) {
+                        return true;
+                    }
+                } else {
+                    map.put(module, i);
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        boolean b = checkSubarraySum(new int[]{23, 2, 3, 6, 7}, 6);
+        System.out.println(b);
+
+    }
+
 }
