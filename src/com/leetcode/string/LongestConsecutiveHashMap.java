bdf6ef77b1facdaec1eb8b01495d1f95abe0a088
Gautam Kumar Pandey <pandeygautam748@gmail.com>
2026-04-08 00:46:42 +0530

    Find Longest Unique string without repeated character

--- src/com/leetcode/string/LongestConsecutiveHashMap.java
+++ src/com/leetcode/string/LongestConsecutiveHashMap.java
@@ -1,4 +1,34 @@
 package com.leetcode.string;
 
-public class LongestCosecutiveHashMap {
+import java.util.HashMap;
+import java.util.IdentityHashMap;
+
+public class LongestConsecutiveHashMap {
+    public static int findLongestConsecutive(String str){
+        HashMap<Character, Integer> map = new HashMap<>();
+        int l = 0;
+        int r=0;
+        int max  = Integer.MIN_VALUE;
+        while(r<str.length()){
+
+            char c = str.charAt(r);
+            if(map.containsKey(c)){
+                if(map.get(c)>=l){
+                    l=map.get(c)+1;
+                }
+            }
+            max=Math.max(max,r-l+1);
+            map.put(c,r);
+            r++;
+
+
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int result = findLongestConsecutive("cabbdzabcd");
+        System.out.println(result);
+    }
+
 }
