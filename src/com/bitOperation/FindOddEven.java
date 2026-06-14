06e9886d7cb6c1db60f9ba45e89129d94e6471a5
Gautam Kumar Pandey <pandeygautam748@gmail.com>
2026-06-14 20:03:28 +0530

    Find Odd and Even using AND Operator

--- src/com/bitOperation/FindOddEven.java
+++ src/com/bitOperation/FindOddEven.java
@@ -1,4 +1,18 @@
 package com.bitOperation;
 
 public class FindOddEven {
+    public static boolean getOddEven(int x){
+        return (x & 1) == 1;
+    }
+
+    public static void main(String[] args) {
+        boolean oddEven = getOddEven(5);
+        if(oddEven){
+            System.out.println("Odd Number");
+        }
+        else {
+            System.out.println("Even Number");
+        }
+    }
+
 }
