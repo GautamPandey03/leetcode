0f4440efef7b6a050f05274911af3b154e12adec
Gautam Kumar Pandey <pandeygautam748@gmail.com>
2026-06-17 04:09:59 +0530

    Find the difference between two string

--- src/com/leetcode/string/FindTheDifference.java
+++ src/com/leetcode/string/FindTheDifference.java
@@ -1,4 +1,20 @@
 package com.leetcode.string;
 
 public class FindTheDifference {
+    public static char findDifference(String s, String t){//a aa
+
+        char result =0;
+        for(char c:s.toCharArray()){
+            result^=c;
+        }
+        for(char d:t.toCharArray()){
+            result^=d;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        char difference = findDifference("abcd", "abcde");
+        System.out.println(difference);
+    }
 }
